95d2b1c5fb7e13b766e5ec32209a3b183d2ddf82
Kerwin <Kerwin_Wu@aseglobal.com>
2022-09-27 08:13:45 +0800

    加入專案檔案。

---
+++ DCT_data_import/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCT_data_import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
