850969b3331d8b8e75765dd1964012d77c1295b3
Brian <bernie7135@gmail.com>
2024-10-20 19:04:52 +0800

    加入recovery rate功能於db_key系統中 - 更新DbKeyObject &

--- DCT_data_import/DbAccess.cs
+++ DCT_data_import/DbAccess.cs
@@ -6,7 +6,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using static DCT_data_import.ApiObject;
-
 namespace DCT_data_import
 {
     public class DbAccess
@@ -18,7 +17,6 @@ namespace DCT_data_import
             long nowTimeStamp = DateTimeOffset.Now.ToUnixTimeSeconds();
             long threeHourAgoTimeStamp = nowTimeStamp - 86400 * day;  // 24小時=86400秒
             int count = 0;
-
             if (mode == "tester")
             {
                 sql = "SELECT COUNT(id) count_id FROM `db_key` WHERE datetime >= " + threeHourAgoTimeStamp;
@@ -27,7 +25,6 @@ namespace DCT_data_import
             {
                 sql = "SELECT COUNT(id) count_id FROM `db_key_ui_status` WHERE  datetime >= " + threeHourAgoTimeStamp;
             }
-
             try
             {
                 // 宣告 Web API body
@@ -42,7 +39,6 @@ namespace DCT_data_import
                 {
                     writeToLog.writeToLog("SELECT `db_key` error! ");
                 }
-
                 if (int.TryParse(response.data[0]["count_id"].ToString(), out count))
                 {
                     return count;
@@ -51,7 +47,6 @@ namespace DCT_data_import
                 {
                     return 0;
                 }
-
             }
             catch (Exception ex)
             {
@@ -59,13 +54,17 @@ namespace DCT_data_import
                 return -1;
             }
         }
-
+        /// <summary>
+        /// 透過db_key table 擷取尚未匯入資料的flag進行匯入
+        /// </summary>
+        /// <param name="webApiClient"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
         public List<DbKeyObject> SelectDbKey(WebApiClient webApiClient, string mode = "")
         {
             List<DbKeyObject> dbKeyList = new List<DbKeyObject>();
             WriteToLog writeToLog = new WriteToLog();
             string sql = "";
-
             if (mode == "tester")
             {
                 sql = "SELECT id, db_key, tester, test_result, fail_pin, check_status FROM `db_key` WHERE `check_status`>0 AND `import_status` =0 AND mail=0;";
@@ -74,7 +73,6 @@ namespace DCT_data_import
             {
                 sql = "SELECT id, db_key, check_status FROM `db_key_ui_status` WHERE  `check_status`>0 AND `import_status` =0 AND mail=0;";
             }
-
             try
             {
                 // 宣告 Web API body
@@ -89,7 +87,6 @@ namespace DCT_data_import
                 {
                     writeToLog.writeToLog("SELECT `db_key` error! ");
                 }
-
                 for (int i = 0; i < response.data.Count; i++)
                 {
                     //Console.WriteLine(response.data[i]["id"].ToString() + ", "+response.data[i]["db_key"].ToString());
@@ -102,24 +99,19 @@ namespace DCT_data_import
                         dbKeyList.Add(new DbKeyObject(int.Parse(response.data[i]["id"].ToString()), response.data[i]["db_key"].ToString(), int.Parse(response.data[i]["check_status"].ToString())));
                     }
                 }
-
-
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 return new List<DbKeyObject>();
             }
-
             return dbKeyList;
         }
-
         public string UpdateDbKeyImportStatus(WebApiClient webApiClient, string dbKey, int tester, int testResult, int failPin, string remark)
         {
             WriteToLog writeToLog = new WriteToLog();
             int importResult = 4 * tester + 2 * testResult + failPin;
             string id, checkStatus, importStatus = "1", mail = "0";
-
             try
             {
                 // 先select 出check status 比對確認結果
@@ -143,13 +135,13 @@ namespace DCT_data_import
                 {
                     return "Fail. There is no infomation which db_key is '" + dbKey + "'";
                 }
-
                 // 檢查確認碼
                 //if (importResult < int.Parse(checkStatus))
                 //{
                 //    importStatus = "0";
                 //}
-                /*else */if (importResult == int.Parse(checkStatus))
+                /*else */
+                if (importResult == int.Parse(checkStatus))
                 {
                     importStatus = "1";
                 }
@@ -157,13 +149,10 @@ namespace DCT_data_import
                 {
                     importStatus = "2";
                     mail = "1";
-
                     // 寫入寄信暫存檔
                     writeToLog.WriteToMailTemp(dbKey + "," + remark);
                 }
                 //importStatus = (importResult.ToString() == checkStatus) ? "1" : "2";
-                
-
                 // 更新 import check 相關資訊
                 pool_excute = new Pool_excute
                 {
@@ -179,24 +168,19 @@ namespace DCT_data_import
                     writeToLog.writeToLog("UPDATE `db_key` error! ");
                     return "Fail. Execution 'update' error: " + response.error;
                 }
-
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 return "Fail. Exception error";
             }
-
-
             return "OK";
         }
-
         public string UpdateDbKeyUiStatusImportStatus(WebApiClient webApiClient, string dbKey, int uiStatus, string remark)
         {
             WriteToLog writeToLog = new WriteToLog();
             int importResult = uiStatus;
             string id, checkStatus, importStatus = "1", mail = "0";
-
             try
             {
                 // 先select 出check status 比對確認結果
@@ -220,13 +204,13 @@ namespace DCT_data_import
                 {
                     return "Fail. There is no infomation which db_key is '" + dbKey + "'";
                 }
-                
                 // 檢查確認碼
                 //if (importResult < int.Parse(checkStatus))
                 //{
                 //    importStatus = "0";
                 //}
-                /*else*/ if (importResult == int.Parse(checkStatus))
+                /*else*/
+                if (importResult == int.Parse(checkStatus))
                 {
                     importStatus = "1";
                 }
@@ -234,15 +218,11 @@ namespace DCT_data_import
                 {
                     importStatus = "2";
                     mail = "1";
-
                     // 寫入寄信暫存檔
                     writeToLog.WriteToMailTemp(dbKey + "," + remark);
                 }
-
-
                 //// 寫入寄信暫存檔
                 //writeToLog.WriteToMailTemp(dbKey + "," + dbKey);
-
                 // 更新 import check 相關資訊
                 pool_excute = new Pool_excute
                 {
@@ -258,18 +238,14 @@ namespace DCT_data_import
                     writeToLog.writeToLog("UPDATE `db_key` error! ");
                     return "Fail. Execution 'update' error: " + response.error;
                 }
-
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 return "Fail. Exception error";
             }
-
-
             return "OK";
         }
-
         public List<DbKeyObject> SelectFailDbKeyResult(WebApiClient webApiClient, string mode = "")
         {
             List<DbKeyObject> dbKeyObject = new List<DbKeyObject>();
@@ -279,20 +255,18 @@ namespace DCT_data_import
             //long threeHourAgoTimeStamp = nowTimeStamp - 10800;  // 3小時=10800秒  3小時前
             long threeHourAgoTimeStamp = nowTimeStamp - 1200;  // 20分鐘前
             //long threeHourAgoTimeStamp = nowTimeStamp + 10800;  // 3小時=10800秒  3小時後
-
             if (mode == "tester")
             {
                 sql = @"SELECT id, db_key, check_status, remark FROM `db_key` WHERE `mail`=0 AND `import_status`=0 AND datetime <= " + threeHourAgoTimeStamp +
-                          @" union ALL 
+                          @" union ALL
                                 SELECT id, db_key, check_status, remark FROM `db_key` WHERE `mail`= 0 AND `import_status`>=2 AND datetime <= " + threeHourAgoTimeStamp;
             }
             else if (mode == "ui_status")
             {
                 sql = @"SELECT id, db_key, check_status, remark FROM `db_key_ui_status` WHERE `mail`=0 AND `import_status`=0 AND datetime <= " + threeHourAgoTimeStamp +
-                          @" union ALL 
+                          @" union ALL
                                 SELECT id, db_key, check_status, remark FROM `db_key_ui_status` WHERE `mail`= 0 AND `import_status` >=2 AND datetime <= " + threeHourAgoTimeStamp;
             }
-
             try
             {
                 Pool_excute pool_excute = new Pool_excute
@@ -305,58 +279,48 @@ namespace DCT_data_import
                 {
                     writeToLog.writeToLog("SELECT `db_key` error! ");
                 }
-
                 for (int i = 0; i < response.data.Count; i++)
                 {
                     //Console.WriteLine(response.data[i]["db_key"].ToString());
                     remark = (response.data[i]["check_status"].ToString() == "0") ? "未更新check status" : response.data[i]["remark"].ToString();
                     dbKeyObject.Add(new DbKeyObject(int.Parse(response.data[i]["id"].ToString()), response.data[i]["db_key"].ToString(), remark));
                 }
-                
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 return dbKeyObject;
             }
-
             return dbKeyObject;
         }
-
         public List<DbKeyObject> SelectFailDbKeyFromFile()
         {
             List<DbKeyObject> dbKeyObject = new List<DbKeyObject>();
-
             string log_path = new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase) + @"\mail_temp.txt").LocalPath;
-            if(File.Exists(log_path))
+            if (File.Exists(log_path))
             {
                 using (StreamReader reader = new StreamReader(log_path))
                 {
                     string line = reader.ReadLine();
-
                     while (line != null)
                     {
                         string[] strSplit = line.Split(',');
-                        if(strSplit.Length>1)
+                        if (strSplit.Length > 1)
                         {
                             dbKeyObject.Add(new DbKeyObject(strSplit[0], strSplit[1]));
                         }
                         line = reader.ReadLine();
                     }
-                    
                 }
             }
             return dbKeyObject;
         }
-
         public string UpdateMail(WebApiClient webApiClient, List<DbKeyObject> dbKeyObject, string mode = "")
         {
             WriteToLog writeToLog = new WriteToLog();
             string sql = "";
             long nowTimeStamp = DateTimeOffset.Now.ToUnixTimeSeconds();
             long threeHourAgoTimeStamp = nowTimeStamp - 10800;  // 3小時=10800秒
-
-
             try
             {
                 foreach (DbKeyObject item in dbKeyObject)
@@ -373,7 +337,6 @@ namespace DCT_data_import
                             "SET mail=1 " +
                             "WHERE `id`='" + item.id + "';";
                     }
-
                     Pool_excute pool_excute = new Pool_excute
                     {
                         pool = Program.POOL_NAME,
@@ -386,17 +349,13 @@ namespace DCT_data_import
                         return "Fail." + response.error;
                     }
                 }
-
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 return "Fail." + ex.ToString();
             }
-
             return "OK.";
-
         }
-
     }
 }
--- DCT_data_import/WebApi/ApiObject.cs
+++ DCT_data_import/WebApi/ApiObject.cs
@@ -4,47 +4,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace DCT_data_import
 {
     public class ApiObject
     {
-
         public class ImportResult
         {
             public int result { get; set; }
             public string messege { get; set; }
-
             public ImportResult(int result, string messege)
             {
                 this.result = result;
                 this.messege = messege;
             }
-
         }
-
         public class DbKeyObject
         {
             public int id { get; set; }
             public string dbKey { get; set; }
+            public int recoveryRate { get; set; }
             public int tester { get; set; }
             public int testResult { get; set; }
             public int failPin { get; set; }
             public int uiStatus { get; set; }
             public int checkStatus { get; set; }
             public string remark { get; set; }
-
             public DbKeyObject(string dbKey, string remark)
             {
                 this.dbKey = dbKey;
-                this.checkStatus = 0;
+                checkStatus = 0;
                 this.remark = remark;
             }
             public DbKeyObject(int id, string dbKey, string remark)
             {
                 this.id = id;
                 this.dbKey = dbKey;
-                this.checkStatus = 0;
+                checkStatus = 0;
                 this.remark = remark;
             }
             public DbKeyObject(int id, string dbKey, int checkStatus)
@@ -52,9 +47,8 @@ namespace DCT_data_import
                 this.id = id;
                 this.dbKey = dbKey;
                 this.checkStatus = checkStatus;
-                this.remark = "";
+                remark = "";
             }
-
             public DbKeyObject(int id, string dbKey, int tester, int testResult, int failPin, int checkStatus)
             {
                 this.id = id;
@@ -63,11 +57,20 @@ namespace DCT_data_import
                 this.testResult = testResult;
                 this.failPin = failPin;
                 this.checkStatus = checkStatus;
-                this.remark = "";
+                remark = "";
+            }
+            public DbKeyObject(int id, string dbKey, int recoveryRate, int tester, int testResult, int failPin, int checkStatus)
+            {
+                this.id = id;
+                this.dbKey = dbKey;
+                this.recoveryRate = recoveryRate;
+                this.tester = tester;
+                this.testResult = testResult;
+                this.failPin = failPin;
+                this.checkStatus = checkStatus;
+                remark = "";
             }
         }
-
-
         public class Pool
         {
             public string pool_name { get; set; }
@@ -77,52 +80,40 @@ namespace DCT_data_import
             public string password { get; set; }
             public string database { get; set; }
         }
-
         public class Pool_signin
         {
             public string username { get; set; }
             public string password { get; set; }
         }
-
-
         public class Pool_excute
         {
             public string pool { get; set; }
             public string query { get; set; }
         }
-
-
         public class Pool_delete
         {
             public string pool { get; set; }
         }
-
         public class Signin_response
         {
             public string token { get; set; }
             public JObject user { get; set; }
         }
-
-
         public class Pool_create_response
         {
             public JValue data { get; set; }
             public string error { get; set; }
         }
-
         public class Pool_get_all_response
         {
             public JObject data { get; set; }
             public string error { get; set; }
         }
-
         public class Pool_excute_response
         {
             public JArray data { get; set; }
             public string error { get; set; }
         }
-
-
         public class Pool_delete_response
         {
             public JValue data { get; set; }
