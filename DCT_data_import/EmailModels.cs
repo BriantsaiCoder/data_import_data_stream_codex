1f6ec32b1dc560d7ddbd78959d5e2edb3cc9e592
D4677 <BrianPC_Tsai@aseglobal.com>
2025-07-10 16:42:44 +0800

    優化EmailModel.cs

--- DCT_data_import/EmailModels.cs
+++ DCT_data_import/EmailModels.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
 using System.Net.NetworkInformation;
 using System.Text;
-using System.Threading.Tasks;
 namespace DCT_data_import
 {
     public class EmailModels
@@ -17,22 +16,24 @@ namespace DCT_data_import
         public List<string> BccList { get; set; }
         public List<string> FileList { get; set; }
         public string SendResult { get; set; }
-        public Boolean SendEmail()
+        public bool SendEmail()
         {
-            int i = 0;
-            IPAddress tIP = IPAddress.Parse("10.12.10.31");
-            Ping tPingControl = new Ping();
-            PingReply tReply = tPingControl.Send(tIP);
-            tPingControl.Dispose();
-            if (tReply.Status == IPStatus.Success)
+            string ipString = string.Empty;
+            IPAddress tIP = IPAddress.Parse(ipString);
+            using (Ping tPingControl = new Ping())
             {
-                string tolog = string.Empty;
-                string cclog = string.Empty;
-                string bcclog = string.Empty;
-                string filelog = string.Empty;
-                try
+                PingReply tReply = tPingControl.Send(tIP);
+                if (tReply.Status != IPStatus.Success)
                 {
-                    MailMessage mailObj = new MailMessage();
+                    SendResult = "Connect email server fail!";
+                    return false;
+                }
+            }
+            try
+            {
+                using (MailMessage mailObj = new MailMessage())
+                using (SmtpClient mysmtp = new SmtpClient(ipString))
+                {
                     //設定編碼
                     mailObj.SubjectEncoding = Encoding.UTF8;
                     //設定標題
@@ -40,87 +41,79 @@ namespace DCT_data_import
                     //設定內文
                     mailObj.Body = Body;
                     //設定寄件人
-                    mailObj.From = new MailAddress("CTRD5900@aseglobal.com", "CTRD Sender", System.Text.Encoding.UTF8);
+                    mailObj.From = new MailAddress("CTRD5900@aseglobal.com", "CTRD Sender", Encoding.UTF8);
                     //設定to名單
-                    for (i = 0; i < ToList.Count(); i++)
+                    for (int i = 0; i < ToList.Count; i++)
                     {
                         mailObj.To.Add(ToList[i]);
-                        if (i == 0)
-                        {
-                            tolog = ToList[i];
-                        }
-                        else
-                        {
-                            tolog = tolog + "," + ToList[i];
-                        }
                     }
                     //設定cc名單
                     if (CCList != null)
                     {
-                        for (i = 0; i < CCList.Count(); i++)
+                        for (int i = 0; i < CCList.Count; i++)
                         {
                             mailObj.CC.Add(CCList[i]);
-                            if (i == 0)
-                            {
-                                cclog = CCList[i];
-                            }
-                            else
-                            {
-                                cclog = cclog + "," + CCList[i];
-                            }
                         }
                     }
                     //設定bcc名單
                     if (BccList != null)
                     {
-                        for (i = 0; i < BccList.Count(); i++)
+                        for (int i = 0; i < BccList.Count; i++)
                         {
                             mailObj.Bcc.Add(BccList[i]);
-                            if (i == 0)
-                            {
-                                bcclog = BccList[i];
-                            }
-                            else
-                            {
-                                bcclog = bcclog + "," + BccList[i];
-                            }
                         }
                     }
                     //設定夾檔
                     if (FileList != null)
                     {
-                        for (i = 0; i < FileList.Count(); i++)
+                        for (int i = 0; i < FileList.Count; i++)
                         {
+                            if (!File.Exists(FileList[i]))
+                            {
+                                SendResult = $"附件檔案不存在: {FileList[i]}";
+                                return false;
+                            }
                             Console.WriteLine(FileList[i]);
-                            mailObj.Attachments.Add(new System.Net.Mail.Attachment(FileList[i]));
-                            if (i == 0)
-                            {
-                                filelog = FileList[i];
-                            }
-                            else
-                            {
-                                filelog = filelog + "," + FileList[i];
-                            }
+                            mailObj.Attachments.Add(new Attachment(FileList[i]));
                         }
                     }
                     //寄信
                     mailObj.IsBodyHtml = true;
-                    SmtpClient mysmtp = new SmtpClient("10.12.10.31");
                     mysmtp.Send(mailObj);
                 }
-                catch (Exception ex)
-                {
-                    SendResult = ex.ToString();
-                    return false;
-                }
+                SendResult = "Sent the email success!";
+                return true;
             }
-            else
+            catch (SmtpException ex)
             {
-                SendResult = "Connect email server fail!";
+                SendResult = $"SMTP 錯誤: {ex.Message}";
                 return false;
             }
-            SendResult = "Sent the email success!";
-            return true;
+            catch (FileNotFoundException ex)
+            {
+                SendResult = $"找不到附件檔案: {ex.FileName}";
+                return false;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                SendResult = $"檔案存取權限不足: {ex.Message}";
+                return false;
+            }
+            catch (ArgumentException ex)
+            {
+                SendResult = $"參數錯誤: {ex.Message}";
+                return false;
+            }
+            catch (InvalidOperationException ex)
+            {
+                SendResult = $"操作無效: {ex.Message}";
+                return false;
+            }
+            catch (Exception ex)
+            {
+                SendResult = $"未預期的錯誤: {ex.Message}";
+                return false;
+            }
         }
     }
 }
